034f2ccd0f59c66b4650210e2762dbdacdb67163
Bruce-Yu2017 <ilywanzi@gmail.com>
2018-01-09 20:16:12 -0800

    five

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [
-  {path: "", component: AppComponent}
+  
+  {path: "", component: TestComponent},
+  {path: "test", component: SecondComponent},
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { SecondComponent } from './second/second.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
