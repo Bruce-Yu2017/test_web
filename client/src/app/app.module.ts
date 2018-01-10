916672fc5b0072c9555c0dad55f208d74e089e7b
Bruce-Yu2017 <ilywanzi@gmail.com>
2018-01-09 21:03:08 -0800

    new

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,8 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { SecondComponent } from './second/second.component';
+import { SocialLoginModule, AuthServiceConfig } from "angular4-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angular4-social-login";
 
 
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("881642856104-gchuqjdr5lgvinqcvlbi1aovp23f8eq6.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("568749596799970")
+  }
+]);
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +28,8 @@ import { SecondComponent } from './second/second.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule.initialize(config),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/test/test.component.ts
+++ client/src/app/test/test.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from "angular4-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider } from "angular4-social-login";
+import { SocialUser } from "angular4-social-login";
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-
-  constructor() { }
+  user: SocialUser;
+  loggedIn: boolean;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+
+      if (this.user !== null) {
+        this._service.social_login(this.user, (res) => {
+
+          console.log(this.user);
+          localStorage.social_user = JSON.stringify(user)
+          if(this.user !== null) {
+            this._service.check_user(this.user, (res) => {
+              if(res == "exist") {
+                console.log("success social login");
+              }
+              else {
+
+                console.log(res);
+                this._router.navigate(["/update"]);
+              }
+            })
+          }
+
+          this._service.social_user = user;
+          this.loggedIn = (user != null);
+        });
+
+      }
+    })
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+
+    
+    
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
   }
 
 }
